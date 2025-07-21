e57d6a1ab3424c15b19d47fb0d9c2a6e23c4dac0
struct_c <struct_c@struct-cs-MacBook-Air.local>
2025-07-21 14:37:38 +0500

    IMMEDIATE UPDATE

--- include/serviettUI.h
+++ include/serviettUI.h
@@ -1,7 +1,15 @@
+// serviettUI.h
 #pragma once
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <vector>
 #include <string>
 #include <functional>
 
 void Text(const std::string& text);
 void Button(const std::string& label, const std::function<void()>& cb);
+void Toggle(const std::string& label, bool& state);
+void TextField(const std::string& placeholder, std::string& state);
+void HStack(const std::function<void()>& cb);
+void NewView(const std::function<void()>& viewFunc);
 void View(const std::function<void()>& viewFunc);
