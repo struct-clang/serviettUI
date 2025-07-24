a44bf99db308deb490d47370d453ee3de0d24350
struct_c <struct_c@struct-cs-MacBook-Air.local>
2025-07-24 21:44:03 +0500

    Image example

--- test/main.cpp
+++ test/main.cpp
@@ -3,6 +3,7 @@
 
 int i = 0;
 bool isOn = false;
+bool imageVisible = true;
 std::string myTextFieldText = "";
 
 void AnotherView() {
@@ -10,26 +11,23 @@ void AnotherView() {
     Button("Click Me", []() {
         std::cout << "Button Clicked!" << std::endl;
     });
+    Toggle("Show image below", imageVisible);
+    if (imageVisible) {
+        Image("Image.png", 256, 256);
+    } else {
+        Text("Image is hidden");
+    }
 }
 
 void ContentView() {
     Text("Hello, World!");
-    Text("Hello, World!");
-    Text("Hello, World!");
-    Text("Hello, World!");
+
     Button("Add +1 to number below", []() {
         i++;
     });
     Text(std::to_string(i));
 
-    Button("Open another View", []() {
-        NewView(AnotherView);
-    });
-
     Toggle("Show text below", isOn);
-    Toggle("Show text below", isOn);
-    Toggle("Show text below", isOn);
-    
 
     if (isOn) {
         Text("Some text");
@@ -41,6 +39,10 @@ void ContentView() {
     HStack([]() {
         Text("Leading");
         Text("Trailing");
+    });
+
+    Button("Open another View", []() {
+        NewView(AnotherView);
     });
 }
 
